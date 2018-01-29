ac27ba7e4503de0937a970007b2be37e85f4e3db
RosyG <rmga2106@gmail.com>
2018-01-29 17:21:06 -0600

    Añadiendo datos al perfil

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 var provider = new firebase.auth.GoogleAuthProvider();
 //Se llama al botón login para darle el evento click.
 $('#login').click(serviceGoogle);
+//Guardando datos en Firebase cuando se da click en Guardar.
+$('#guardar').click(saveFirebase);
 
 function serviceGoogle () {
   firebase.auth()//Mandando a llamar a Firebase.
@@ -10,11 +12,50 @@ function serviceGoogle () {
     //En este momento el us ya accedio.
     $('.login').hide();//Ocultado la ventana de logear.
     $('#welcome').show('slow');//Mostrando la pantalla principal.
-    //Ejecutando Modal
-    $('#modal1').modal()
-    //Accediendo a la información del us,ie, result.
-    var imgUser = result.user.photoURL;
-    console.log(imgUser);
-    $('#img-us').append("<img src='"+imgUser+"' />")
+    $('#welcome-user').text('Bienvenid@ ' + result.user.displayName);//Añadiendo nombre de usuario para concatenar.
+    //Guardando la información del usuario en la base de datos de Firebase.
+//    saveUs (result.user);//Guada la información del usuario de manera automatica.
+    paintProfile (result.user);//Pinta los datos del usuario en su perfil.
   });
 };
+//Función que guarda automaticamente.
+function saveUs(user) {
+//  saveText();
+  console.log(publications);
+  var InfUser = {
+    uid:user.uid,
+    name:user.displayName,
+    email:user.email,
+    photo:user.photoURL,
+    sexo: 'Fem'
+  }
+  firebase.database().ref('usuarios/'+user.uid)//Se guarda en la rama que tiene al identificador unico UID.
+    .set(InfUser);//Set modifica a la llave especificada por el uid, push() solo agreda de nuevo.
+}
+
+function paintProfile (user) {
+  var imgUser = user.photoURL;//Variable que contine toda la inf del usuario.
+  var nameUser = user.displayName;
+  var email = user.email;
+  var uid= user.uid;
+  console.log(imgUser);
+  $('#container-profile').append("<img  class = 'img-us' src = '"+imgUser+"' />")
+  console.log(nameUser);
+  console.log(email);
+  $('#name').text(nameUser);
+  $('#email').text(email);
+}
+
+//Función que guarda datos al hacer click en guardar.
+function saveFirebase () {
+  firebase.database().ref('us1')
+    .set({
+          edad:'15años',
+          sexo: 'Feme'
+          })//Escribiendo en la base de datos.
+}
+/*function saveText () {
+  var publications = $.post['text'];
+  console.log(publications);
+
+}*/
